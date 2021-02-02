841ee93d2abfd6005a86c003a3e5c6e0127d3084
phart <phart@isp02.tacc.utexas.edu>
2021-02-02 11:37:38 -0600

    started tracking json example script

---
+++ json_ex.py
@@ -0,0 +1,34 @@
+import json
+
+def check_char_count(mystr):
+	
+	assert isinstance(mystr, str), 'Input to this function should be a string'
+
+	if(len(mystr) == 2):
+		return(f'{mystr} count passes')
+	else:
+		return(f'{mystr} count FAILS')
+
+def check_char_type(mystr):
+    if (mystr.isalpha() and mystr.isupper()):
+        return( f'{mystr} type passes' )
+    else:
+        return( f'{mystr} type FAILS' )
+
+def check_first_letter(mystr, mystate):
+	if(mystr[0] == mystate[0]):
+		return(f'{mystr} letter passes')
+	else:
+		return(f'{mystr} letter FAILS')
+def main():
+	with open('states.json', 'r') as f:
+		states = json.load(f)
+
+	for i in range(50):
+		print(check_char_count(states['states'][i]['abbreviation']))
+		print(check_char_type(states['states'][i]['abbreviation']))
+		print(check_first_letter(states['states'][i]['abbreviation'], states['states'][i]['name']))
+
+
+if __name__ == '__main__':
+    main()
